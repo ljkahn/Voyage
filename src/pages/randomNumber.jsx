1945cca2d31cc5d36db11b30fe39003e2b9343d8
ljkahn <lia.j.kahn@gmail.com>
2023-12-16 12:35:23 -0700

    added linter and random number page

--- src/pages/randomNumber.jsx
+++ src/pages/randomNumber.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function randomNumber() {
-  return <div>randomNumber</div>;
+function RandomNumber() {
+  const [randomYear, setRandomYear] = useState(null);
+
+  const generateRandomYear = () => {
+    const minYear = 1900;
+    const maxYear = 2023;
+    const randomYear =
+      Math.floor(Math.random() * (maxYear - minYear + 1)) + minYear;
+    setRandomYear(randomYear);
+  };
+
+  return (
+    <div>
+      <h1>Random Year Generator</h1>
+      <button onClick={generateRandomYear}>Generate Random Year</button>
+      {randomYear && <div>Random Year: {randomYear}</div>}
+    </div>
+  );
 }
 
-export default randomNumber;
+export default RandomNumber;
